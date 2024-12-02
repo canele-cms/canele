1f678280ead4e5e602b24752ae50ea12bab90a60
Isaac Graves <isaac.graves@thetradedesk.com>
2024-12-01 19:46:38 -0800

    updateConfig

--- packages/astro/src/integration.ts
+++ packages/astro/src/integration.ts
@@ -1,10 +1,24 @@
 import type { AstroIntegration } from "astro";
+import { envField } from "astro/config";
 
 export default function caneleAstroIntregration(): AstroIntegration {
   return {
     name: "canele",
     hooks: {
-      "astro:config:setup": ({ injectRoute, injectScript, addMiddleware }) => {
+      "astro:config:setup": ({ injectRoute, addMiddleware, updateConfig }) => {
+        updateConfig({
+          security: {
+            checkOrigin: true,
+          },
+          env: {
+            schema: {
+              GITHUB_REPO: envField.string({ access: "secret", context: "server" }),
+              GITHUB_CLIENT_ID: envField.string({ access: "secret", context: "server" }),
+              GITHUB_CLIENT_SECRET: envField.string({ access: "secret", context: "server" }),
+            },
+          },
+        });
+
         addMiddleware({
           order: "pre",
           entrypoint: "@canele/astro/middleware",
@@ -27,6 +41,7 @@ export default function caneleAstroIntregration(): AstroIntegration {
           entrypoint: "@canele/astro/pages/canele/github/api",
         });
       },
+
       "astro:config:done": ({ injectTypes }) => {
         injectTypes({
           filename: "locals.d.ts",
